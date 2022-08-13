6e2fcf26adca4b93a1f54ad31334521a369f44d5
mstepanov <stepanov_maks@mail.ru>
2022-08-13 12:05:23 +0700

    Added reusing shared dependencies when building graph

--- src/main/kotlin/com/github/maxstepanovski/projecttreeplugin/graph/StandardGraphBuilder.kt
+++ src/main/kotlin/com/github/maxstepanovski/projecttreeplugin/graph/StandardGraphBuilder.kt
@@ -11,16 +11,20 @@ class StandardGraphBuilder(
 
     override fun buildGraph(className: String): ClassWrapper? {
         val rootNode: ClassWrapper = classResolver.resolveClassByName(className) ?: return null
+        val resolved = mutableMapOf<String, ClassWrapper>()
         val deque = ArrayDeque<ClassWrapper>()
         deque.addLast(rootNode)
 
         while (deque.isNotEmpty()) {
             val node = deque.removeFirst()
-            (node.constructorParameters + node.fields).forEach {
-                classResolver.resolveClassByFullName(it.fullName)?.let { childNode ->
-                    node.addDependency(childNode)
-                    deque.addLast(childNode)
-                }
+            (node.constructorParameters + node.fields).forEach { dependency ->
+                val fullName = dependency.fullName
+                (resolved[fullName] ?: (classResolver.resolveClassByFullName(fullName)
+                        ?.also { resolved[fullName] = it }))
+                        ?.let { childNode ->
+                            node.addDependency(childNode)
+                            deque.addLast(childNode)
+                        }
             }
         }
 
