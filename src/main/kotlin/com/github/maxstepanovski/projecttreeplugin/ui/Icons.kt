66156d666d3726ff7444709fa0c33aacca8233d7
mstepanov <maksim.stepanov@agoda.com>
2022-10-12 17:30:46 +0700

    Support Intellij Idea 222.* Added action icon

---
+++ src/main/kotlin/com/github/maxstepanovski/projecttreeplugin/ui/Icons.kt
@@ -0,0 +1,8 @@
+package com.github.maxstepanovski.projecttreeplugin.ui
+
+import com.intellij.openapi.util.IconLoader
+
+object Icons {
+    @JvmField
+    val actionIcon = IconLoader.getIcon("/icons/actionIcon.png", javaClass)
+}
