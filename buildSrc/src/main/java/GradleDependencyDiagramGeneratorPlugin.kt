7b916feecfe601818d4bea4df43c00d1b2c8b509
oogbe <ozioma.ogbe@agoda.com>
2022-11-21 18:53:23 +0700

    remove build src project.

--- buildSrc/src/main/java/GradleDependencyDiagramGeneratorPlugin.kt
+++
@@ -1,8 +0,0 @@
-//import org.gradle.api.Plugin
-//import org.gradle.api.Project
-//
-//class GradleDependencyDiagramGeneratorPlugin : Plugin<Project> {
-//    override fun apply(project: Project) {
-//        project.tasks.register("generateGradleDependencyReport", DependencyReportGenerator::class.java)
-//    }
-//}
