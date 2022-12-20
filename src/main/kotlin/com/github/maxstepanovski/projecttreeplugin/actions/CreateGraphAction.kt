a2676418020bae738684729b30badddec9e19963
Ozioma Ogbe <ogbeoziomajnr@yahoo.com>
2022-12-20 17:00:46 +0700

    generate graph from the background.

--- src/main/kotlin/com/github/maxstepanovski/projecttreeplugin/actions/CreateGraphAction.kt
+++ src/main/kotlin/com/github/maxstepanovski/projecttreeplugin/actions/CreateGraphAction.kt
@@ -10,9 +10,14 @@ import com.github.maxstepanovski.projecttreeplugin.parser.ParsingInteractorImpl
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.application.AccessToken
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.project.Project
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiFile
@@ -21,13 +26,15 @@ import com.intellij.psi.PsiManager
 import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.kotlin.psi.KtFile
 import java.nio.file.Path
+import com.intellij.openapi.progress.runBackgroundableTask
+import javax.swing.SwingUtilities
+
 
 class CreateGraphAction : AnAction(), ClassResolver {
     private val graphBuilder: GraphBuilder = BfsGraphBuilder(this)
     private val parsingInteractor: ParsingInteractor = ParsingInteractorImpl()
 
     private lateinit var file: PsiFile
-    private lateinit var project: Project
     private lateinit var psiFacade: JavaPsiFacade
     private lateinit var psiManager: PsiManager
     private lateinit var globalSearchScope: GlobalSearchScope
@@ -36,45 +43,60 @@ class CreateGraphAction : AnAction(), ClassResolver {
     private lateinit var diagramRepository: DiagramRepository
 
     override fun actionPerformed(e: AnActionEvent) {
-        // initialization
-        file = e.getData(CommonDataKeys.PSI_FILE) ?: return
-        project = e.project ?: return
-        psiFacade = JavaPsiFacade.getInstance(project)
-        psiManager = PsiManager.getInstance(project)
-        globalSearchScope = GlobalSearchScope.projectScope(project)
-        editor = e.getData(CommonDataKeys.EDITOR) ?: return
-        vfManager = VirtualFileManager.getInstance()
-        diagramRepository = DiagramRepository(project)
+        runBackgroundableTask("Generating Dependency Graph", e.project, true) {
+            val indicator = it
+            val project = e.project ?: return@runBackgroundableTask
 
-        // get name of the element with caret(cursor) on it.
-        // caret should be positioned on class name,
-        // otherwise unclear which class in the file should be the root (if more than 1 class in file)
-        val shortName: String = file.findElementAt(editor.caretModel.offset)?.text ?: return
-        val packageName = when (file) {
-            is PsiJavaFile -> (file as PsiJavaFile).packageName
-            is KtFile -> (file as KtFile).packageName
-            else -> ""
-        }
-        val fullName = "${packageName}.${shortName}"
+            // initialization
+            file = e.getData(CommonDataKeys.PSI_FILE) ?: return@runBackgroundableTask
+            psiFacade = JavaPsiFacade.getInstance(project)
+            psiManager = PsiManager.getInstance(project)
+            globalSearchScope = GlobalSearchScope.projectScope(project)
+            editor = e.getData(CommonDataKeys.EDITOR) ?: return@runBackgroundableTask
+            vfManager = VirtualFileManager.getInstance()
+            diagramRepository = DiagramRepository(project)
 
-        // if file doesn't exist, create class dependencies graph and save it to the file
-        if (diagramRepository.shouldCreateFile(fullName)) {
-            val rootNode: ClassWrapper = graphBuilder.buildGraph(fullName) ?: return
-            diagramRepository.saveToFile(rootNode)
-        }
+            // get name of the element with caret(cursor) on it.
+            // caret should be positioned on class name,
+            // otherwise unclear which class in the file should be the root (if more than 1 class in file)
+            val shortName: String = file.findElementAt(editor.caretModel.offset)?.text ?: return@runBackgroundableTask
+            val packageName = when (file) {
+                is PsiJavaFile -> (file as PsiJavaFile).packageName
+                is KtFile -> (file as KtFile).packageName
+                else -> ""
+            }
+            val fullName = "${packageName}.${shortName}"
 
-        // open an editor tab for given file
-        vfManager.refreshAndFindFileByNioPath(Path.of(diagramRepository.getFilePath(shortName)))?.let {
-            FileEditorManager.getInstance(project).openFile(it, false)
+            // if file doesn't exist, create class dependencies graph and save it to the file
+            try {
+                ApplicationManager.getApplication().runReadAction {
+                    if (diagramRepository.shouldCreateFile(fullName)) {
+                        indicator.text = "Creating dependency graph and saving to file"
+                        val rootNode: ClassWrapper = graphBuilder.buildGraph(fullName) ?: return@runReadAction
+                        diagramRepository.saveToFile(rootNode)
+                    }
+
+
+                    // open an editor tab for given file
+                }
+            } catch (ex: Exception) {
+                indicator.cancel()
+                ex.printStackTrace()
+            }
+            indicator.text = "Opening graph in editor"
+
+            vfManager.refreshAndFindFileByNioPath(Path.of(diagramRepository.getFilePath(shortName)))?.let {
+                SwingUtilities.invokeLater {
+                    FileEditorManager.getInstance(project).openFile(it, false)
+                }
+            }
         }
     }
 
     override fun resolveClassByFullName(fullName: String): ClassWrapper? =
-            psiFacade.findClass(fullName, globalSearchScope)
-                ?.let {
-                    psiManager.findFile(it.containingFile.virtualFile)
-                }
-                ?.let {
-                    parsingInteractor.parseFile(it, fullName.split(".").last())
-                }
+        psiFacade.findClass(fullName, globalSearchScope)?.let {
+                psiManager.findFile(it.containingFile.virtualFile)
+            }?.let {
+                parsingInteractor.parseFile(it, fullName.split(".").last())
+            }
 }
